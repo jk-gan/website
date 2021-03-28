b99956ceec225e79c58a6ac405af499786aa9b2d
jk-gan <junkai@hey.com>
2021-03-28 12:54:21 +0800

    feat: add blog post list page
    Signed-off-by: jk-gan <junkai@hey.com>

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../../components/bio"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 
@@ -9,56 +8,62 @@ const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
-    return (
-      <Layout location={location} title={siteTitle}>
-        <SEO title="All posts" />
-        <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
-      </Layout>
-    )
-  }
-
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
-      <Bio />
-      <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
+      <div className="flex container mx-auto mt-16 w-11/12 2xl:w-5/12 xl:w-6/12 lg:w-8/12 md:w-10/12">
+        <div className="divide-y-2 w-full">
+          <div>
+            <h1 className="text-left font-bold text-4xl mb-3">Writings</h1>
+          </div>
+          <div>
+            <ul className="mt-8">
+              {posts.map(post => {
+                const title = post.frontmatter.title || post.fields.slug
 
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
+                return (
+                  <li className="mb-10" key={post.fields.slug}>
+                    <Link href={`/blog/${post.fields.slug}`}>
+                      <a className="text-2xl mb-2 font-semibold hover:text-cyan-400">
+                        {title}
+                      </a>
                     </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
-      </ol>
+                    <p className="text-base text-blueGray-500 mb-2">
+                      {/* {post.subtitle || "..."} */}
+                      <section>
+                        <p
+                          dangerouslySetInnerHTML={{
+                            __html:
+                              post.frontmatter.description || post.excerpt,
+                          }}
+                          itemProp="description"
+                        />
+                      </section>
+                    </p>
+                    {/* <ul className="flex flex-wrap mb-2">
+                      {post.tags.map(tag => {
+                        return (
+                          <li key={tag}>
+                            <Link href={`/tags/${tag}`}>
+                              <a className="text-sm mr-2 py-1 px-2 rounded-xl bg-blueGray-100 hover:bg-blueGray-200">
+                                #{tag}
+                              </a>
+                            </Link>
+                          </li>
+                        )
+                      })}
+                    </ul> */}
+                    <p className="text-sm text-blueGray-400">
+                      {/* {dayjs(post.date).format("MMMM D, YYYY")} */}
+                      {post.frontmatter.date}
+                    </p>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
