dd6da14f205e169253673dea6d721e720c9f22dc
jk-gan <junkai@hey.com>
2021-03-28 14:01:14 +0800

    feat: add contact page
    Signed-off-by: jk-gan <junkai@hey.com>

---
+++ src/pages/contact.js
@@ -0,0 +1,42 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Contact = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="Home page" />
+      <div className="flex container mx-auto mt-16 w-11/12 2xl:w-5/12 xl:w-6/12 lg:w-8/12 md:w-10/12">
+        <div>
+          <h1 className="text-left font-bold text-3xl mb-10">
+            Let's grab a coffee
+          </h1>
+          <p className="text-left text-xl text-blueGray-800 mb-3">
+            If you like my works or want to connect with me, never hesitate to
+            send me an email at{" "}
+            <a className="link" href="mailto:junkai@hey.com">
+              junkai@hey.com
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
+export default Contact
